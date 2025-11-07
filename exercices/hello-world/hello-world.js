86b8d53b8e162f3a7b92d3f271c67a0ac305bedb
Thomas 🎩 <pk84vtk@eduvaud.ch>
2025-11-07 14:01:01 +0100

    feat(Hello-World):Terminer l'exercice [10][Done]

--- exercices/hello-world/hello-world.js
+++ exercices/hello-world/hello-world.js
@@ -2,7 +2,8 @@
 // Ceci est uniquement le SQUELETTE de l’exercice « Hello World ».
 // Il t’est fourni pour te permettre de démarrer plus rapidement l’écriture du code.
 //
+const helloworld = "Hello, World!"
 
 export function hello() {
-  throw new Error("Remove this line and implement the function");
+  return helloworld
 }
