f34b5421c226fb6629700dab61df7b28c766d8d2
Thomas 🎩 <pk84vtk@eduvaud.ch>
2025-11-21 16:07:59 +0100

    feat(fruit-picker):Terminer l'exercice
    [38][Done]

--- exercices/fruit-picker/fruit-picker.js
+++ exercices/fruit-picker/fruit-picker.js
@@ -16,14 +16,15 @@ import { order } from './grocer';
  * @return void
  */
 export function onSuccess() {
-  // implement the onSuccess callback to call notify with a success message
+  notify( { message: 'SUCCESS' })
+  
 }
 
 /**
  * @return void
  */
 export function onError() {
-  // implement the onError callback to call notify with an error message
+  notify( { message: 'ERROR' })
 }
 
 /**
@@ -33,7 +34,8 @@ export function onError() {
  * @return void
  */
 export function orderFromGrocer(query, onSuccessCallback, onErrorCallback) {
-  // implement the orderFromGrocer function to order the query
+  order(query, onSuccessCallback, onErrorCallback)
+
 }
 
 /**
@@ -42,5 +44,5 @@ export function orderFromGrocer(query, onSuccessCallback, onErrorCallback) {
  * @return void
  */
 export function postOrder(variety, quantity) {
-  //implement the postOrder function to create a query and order
+  order({"quantity": quantity, "variety": variety}, onSuccess, onError)
 }
