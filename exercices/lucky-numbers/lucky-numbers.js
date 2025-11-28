8f6e7c7545deeafc832785e297f62bcfceb3607e
Thomas 🎩 <pk84vtk@eduvaud.ch>
2025-11-28 15:20:11 +0100

    feat(lucky-numbers):Terminer l'exercice
    [169][Done]

--- exercices/lucky-numbers/lucky-numbers.js
+++ exercices/lucky-numbers/lucky-numbers.js
@@ -8,7 +8,23 @@
  * @returns {number} sum of the two arrays
  */
 export function twoSum(array1, array2) {
-  throw new Error('Remove this line and implement the function');
+  let length = array1.length - 1;
+  let test = 0;
+  let test2 = 0;
+  let length2 = array2.length - 1;
+  array1.forEach((element) => {
+    let number = Math.pow(10, length);
+    element = element * number;
+    test = test + element;
+    length--;
+  });
+  array2.forEach((element2) => {
+    let number2 = Math.pow(10, length2);
+    element2 = element2 * number2;
+    test2 = test2 + element2;
+    length2--;
+  });
+  return test2 + test;
 }
 
 /**
@@ -18,7 +34,24 @@ export function twoSum(array1, array2) {
  * @returns {boolean} whether the number is a palindrome or not
  */
 export function luckyNumber(value) {
-  throw new Error('Remove this line and implement the function');
+  let reverseNum = 0;
+  let temp = value;
+
+  while (temp != 0) {
+    let digit = temp % 10;
+    let number = Math.floor(digit);
+    if (number == 0) {
+      break;
+    } else {
+      reverseNum = reverseNum * 10 + number;
+      temp = temp / 10;
+    }
+  }
+  if (value == reverseNum) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 /**
@@ -29,5 +62,13 @@ export function luckyNumber(value) {
  * @returns {string} error message
  */
 export function errorMessage(input) {
-  throw new Error('Remove this line and implement the function');
+  if (Boolean(input)) {
+    if (!Number(input)) {
+      return "Must be a number besides 0";
+    } else {
+      return "";
+    }
+  } else {
+    return "Required field";
+  }
 }
