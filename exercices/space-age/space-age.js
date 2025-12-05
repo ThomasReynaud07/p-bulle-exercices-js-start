094fa43c3f93c389ec6d5f4195739cd17d24079e
Thomas 🎩 <pk84vtk@eduvaud.ch>
2025-12-05 14:43:14 +0100

    feat(space-age): Terminer l'exercice
    [46][Done]

--- exercices/space-age/space-age.js
+++ exercices/space-age/space-age.js
@@ -3,6 +3,33 @@
 // convenience to get you started writing code faster.
 //
 
-export const age = () => {
-  throw new Error('Remove this line and implement the function');
+export const age = (planet, age) => {
+  const earthTime = 1.0;
+  const earth = 31557600;
+  const mercury = 0.2408467;
+  const venus = 0.61519726;
+  const mars = 1.8808158;
+  const jupiter = 11.862615;
+  const saturne = 29.447498;
+  const uranus = 84.016846;
+  const neptune = 164.79132;
+  if (planet == "earth") {
+    return Number((age / earth / earthTime).toFixed(2));
+  } else if (planet == "mercury") {
+    return Number((age / earth / mercury).toFixed(2));
+  } else if (planet == "venus") {
+    return Number((age / earth / venus).toFixed(2));
+  } else if (planet == "mars") {
+    return Number((age / earth / mars).toFixed(2));
+  } else if (planet == "jupiter") {
+    return Number((age / earth / jupiter).toFixed(2));
+  } else if (planet == "saturn") {
+    return Number((age / earth / saturne).toFixed(2));
+  } else if (planet == "uranus") {
+    return Number((age / earth / uranus).toFixed(2));
+  } else if (planet == "neptune") {
+    return Number((age / earth / neptune).toFixed(2));
+  } else {
+    throw new Error("not a planet");
+  }
 };
