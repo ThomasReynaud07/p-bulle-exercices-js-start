8608aa2d2231acd60c93f6be51d2c798a852d303
Thomas 🎩 <pk84vtk@eduvaud.ch>
2025-12-05 13:27:41 +0100

    feat(gigasecond): Terminer l'exercice
    [67][Done]

--- exercices/gigasecond/gigasecond.js
+++ exercices/gigasecond/gigasecond.js
@@ -3,10 +3,6 @@
 // convenience to get you started writing code faster.
 //
 
-export const gigasecond = () => {
-  let secondes = 1000000000;
-  let date = new Date(
-    gigasecond.setSeconds(gigasecond.getSeconds() + secondes)
-  );
-  return date;
+export const gigasecond = (date) => {
+  return new Date(date.getTime() + 1e12);
 };
