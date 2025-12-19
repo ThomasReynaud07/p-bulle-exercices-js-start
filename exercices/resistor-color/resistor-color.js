6b4597144b9b56d9a861bb6e8e5f2a579c0042c7
Thomas <pk84vtk@eduvaud.ch>
2025-12-19 15:37:26 +0100

    feat(resistor-color):Terminer l'exercice
    [21][Done]

--- exercices/resistor-color/resistor-color.js
+++ exercices/resistor-color/resistor-color.js
@@ -3,8 +3,50 @@
 // convenience to get you started writing code faster.
 //
 
-export const colorCode = () => {
-  throw new Error('Remove this line and implement the function');
+export const colorCode = (color) => {
+  switch (color) {
+    case "black":
+      return 0;
+      break;
+    case "brown":
+      return 1;
+      break;
+    case "red":
+      return 2;
+      break;
+    case "orange":
+      return 3;
+      break;
+    case "yellow":
+      return 4;
+      break;
+    case "green":
+      return 5;
+      break;
+    case "blue":
+      return 6;
+      break;
+    case "violet":
+      return 7;
+      break;
+    case "grey":
+      return 8;
+      break;
+    case "white":
+      return 9;
+      break;
+  }
 };
 
-export const COLORS = undefined;
+export const COLORS = [
+  "black",
+  "brown",
+  "red",
+  "orange",
+  "yellow",
+  "green",
+  "blue",
+  "violet",
+  "grey",
+  "white",
+];
